7e8a8e4faa46c5c1539297b38575a7f8716dcfdc
pbutin <pierrick.butin.v2@gmail.com>
2019-11-26 21:54:34 +0100

    map theme

---
+++ js.js
@@ -0,0 +1,6 @@
+window.onload = function init() { 
+	 /* const response = fetch('https://www.marinetraffic.com/en/ais/details/ships/shipid:257330/mmsi:245136000/imo:5241659/vessel:MORGENSTER');
+	  const text = response.text();
+	  const dom = new JSDOM(text);
+	  console.log(dom.window.document.querySelector("strong").textContent);*/
+} 
